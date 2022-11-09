891833b4fbf5ed91dc5674caed221a37bfc69f42
grecia.anicama@gmail.com <grecia.anicama@gmail.com>
2022-11-08 20:39:23 -0500

    subiendo archivo2

---
+++ d2dvq461rdwooi.cloudfront.net/elcomercio/post/videos/spaces303f.js
@@ -0,0 +1,32 @@
+var adsColl = [
+    {
+        "bloque": "1",
+        "dimensions": ['fluid',[728,90]],
+        "dimensions_mobile": ['fluid',[320,50]],
+        "dispositivo": "desktop,mobile",
+        "id": "gpt_zocalo",
+        "id_sitio": 1,
+        "islazyload": false,
+        "num_segundos_refresh": 30,
+        "refresh": true,
+        "slotName": "elcomercio/web/post/videos/zocalo",
+        "slotName2": "elcomercio/web/post/videos/zocalo",
+        "space": "zocalo",
+        "url": "elcomercio/post/videos"
+    },
+    {
+        "bloque": "1",
+        "dimensions": ['fluid',[728,90],[970,90]],
+        "dimensions_mobile": ['fluid',[320,50],[320,60],[320,80]],
+        "dispositivo": "desktop,mobile",
+        "id": "gpt_top",
+        "id_sitio": 1,
+        "islazyload": true,
+        "num_segundos_refresh": 30,
+        "refresh": false,
+        "slotName": "elcomercio/web/post/videos/top",
+        "slotName2": "elcomercio/web/post/videos/top",
+        "space": "top",
+        "url": "elcomercio/post/videos"
+    }
+]
